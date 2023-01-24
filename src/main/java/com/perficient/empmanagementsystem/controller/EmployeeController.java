0dd380838c02eff4f5f5e0206c73ffd6fa47e084
demoapplctn <ramyashettym246@gmail.com>
2023-01-24 12:56:29 +0530

    create packages

---
+++ src/main/java/com/perficient/empmanagementsystem/controller/EmployeeController.java
@@ -0,0 +1,12 @@
+package com.perficient.empmanagementsystem.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin("*")
+@RestController
+@RequestMapping("/emp")
+public class EmployeeController {
+
+}
