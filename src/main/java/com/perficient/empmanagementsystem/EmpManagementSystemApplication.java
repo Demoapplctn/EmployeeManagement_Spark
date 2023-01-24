3d82521acbd5fe601a7d87f043efa452778eeb68
demoapplctn <ramyashettym246@gmail.com>
2023-01-24 12:39:11 +0530

    Initial version

---
+++ src/main/java/com/perficient/empmanagementsystem/EmpManagementSystemApplication.java
@@ -0,0 +1,14 @@
+package com.perficient.empmanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpManagementSystemApplication {
+
+	public static void main(String[] args) {
+		System.out.println("start");
+		SpringApplication.run(EmpManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/perficient/empmanagementsystem/EmpManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perficient.empmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
